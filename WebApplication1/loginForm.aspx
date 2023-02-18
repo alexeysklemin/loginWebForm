cf7b0090e3ab59be167538fbe518e9a7b87d1c68
a_skl <a_skl@LAPTOP-ALEX>
2023-02-18 20:05:14 +0400

    Add project files.

---
+++ WebApplication1/loginForm.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="loginForm.aspx.cs" Inherits="WebApplication1.loginForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="lbusername" runat="server" Text="user name"></asp:Label>
+        <asp:TextBox ID="txtuser" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="lbPswd" runat="server" Text="Password"></asp:Label>
+            <asp:TextBox ID="txtPswd" TextMode="Password" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Enabled="False" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" Width="242px" OnClick="btnLogin_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/loginForm.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WebApplication1
+{
+    public partial class loginForm : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection ("Data Source=LAPTOP-ALEX\\SQLEXPRESS;Initial Catalog=emloees;Integrated Security=True");
+        //Data Source=laptop-alex\sqlexpress;Initial Catalog=emloees;Integrated Security=True
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM loginTable WHERE account='"+ txtuser.Text + "'AND passsword="+ txtPswd+'"',conn);
+            DataTable DaTab = new DataTable();
+             sqlDataAdapter.Fill(DaTab);
+
+            if (DaTab.Rows.Count > 0)
+            {
+                Label1.Text = "You successfuly login to the system";
+            }
+            else
+            {
+                Label1.Text = "Invalid UserName or password";
+            }
+        }
+    }
+}
